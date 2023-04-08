1fa561cb163cc5632a46b72838963a1bc3e973b4
Cedric Hutchings <ced@misguided.enterprises>
2023-04-08 12:41:37 -0400

    shadow

--- index.js
+++ index.js
@@ -9,23 +9,12 @@ function initBuffers(gl) {
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
     const positions = [
-      // Front face
-      -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0, 1.0,
-
-      // Back face
-      -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0, -1.0,
-
-      // Top face
-      -1.0, 1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0,
-
-      // Bottom face
-      -1.0, -1.0, -1.0, 1.0, -1.0, -1.0, 1.0, -1.0, 1.0, -1.0, -1.0, 1.0,
-
-      // Right face
-      1.0, -1.0, -1.0, 1.0, 1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 1.0,
-
-      // Left face
-      -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
+      -1.0, 0.0,  1.0,  1.0, 0.0,  1.0,  1.0, 2.0,  1.0, -1.0, 2.0,  1.0, // Front face
+      -1.0, 0.0, -1.0, -1.0, 2.0, -1.0,  1.0, 2.0, -1.0,  1.0, 0.0, -1.0, // Back face
+      -1.0, 2.0, -1.0, -1.0, 2.0,  1.0,  1.0, 2.0,  1.0,  1.0, 2.0, -1.0, // Top face
+      -1.0, 0.0, -1.0,  1.0, 0.0, -1.0,  1.0, 0.0,  1.0, -1.0, 0.0,  1.0, // Bottom face
+       1.0, 0.0, -1.0,  1.0, 2.0, -1.0,  1.0, 2.0,  1.0,  1.0, 0.0,  1.0, // Right face
+      -1.0, 0.0, -1.0, -1.0, 0.0,  1.0, -1.0, 2.0,  1.0, -1.0, 2.0, -1.0, // Left face
     ];
 
     // Now pass the list of positions into WebGL to build the
@@ -44,22 +33,18 @@ function initBuffers(gl) {
     // indices into the vertex array to specify each triangle's
     // position.
 
-    const indices = [
+    const indices = new Uint16Array([
        0,  1,  2,  0,  2,  3, // front
        4,  5,  6,  4,  6,  7, // back
        8,  9, 10,  8, 10, 11, // top
       12, 13, 14, 12, 14, 15, // bottom
       16, 17, 18, 16, 18, 19, // right
       20, 21, 22, 20, 22, 23, // left
-    ];
+    ]);
 
     // Now send the element array to GL
 
-    gl.bufferData(
-      gl.ELEMENT_ARRAY_BUFFER,
-      new Uint16Array(indices),
-      gl.STATIC_DRAW
-    );
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
 
     return indexBuffer;
   }
@@ -84,35 +69,21 @@ function drawScene(gl, programInfo, buffers, { color, scale, cubeRotation }) {
 
   const modelViewMatrix = mat4.create();
 
-  mat4.translate(
-    modelViewMatrix, // destination matrix
-    modelViewMatrix, // matrix to translate
-    [-0.0, 0.0, -6.0]
-  ); // amount to translate
+  {
+    const x = Math.cos(cubeRotation) * 6;
+    const y = Math.sin(cubeRotation) * 6;
+    mat4.lookAt(
+      modelViewMatrix, // destination matrix
+      [   x, 4.0,    y],
+      [-0.0, 0.0, -0.0],
+      [ 0.0, 1.0,  0.0],
+    ); // amount to translate
+  }
 
   mat4.scale(
     modelViewMatrix, // destination matrix
     modelViewMatrix, // matrix to rotate
-    [scale, scale, scale]
-  ); // axis to rotate around (X)
-
-  mat4.rotate(
-    modelViewMatrix, // destination matrix
-    modelViewMatrix, // matrix to rotate
-    cubeRotation, // amount to rotate in radians
-    [0, 0, 1]
-  ); // axis to rotate around (Z)
-  mat4.rotate(
-    modelViewMatrix, // destination matrix
-    modelViewMatrix, // matrix to rotate
-    cubeRotation * 0.7, // amount to rotate in radians
-    [0, 1, 0]
-  ); // axis to rotate around (Y)
-  mat4.rotate(
-    modelViewMatrix, // destination matrix
-    modelViewMatrix, // matrix to rotate
-    cubeRotation * 0.3, // amount to rotate in radians
-    [1, 0, 0]
+    scale
   ); // axis to rotate around (X)
 
   // Tell WebGL how to pull out the positions from the position
@@ -167,9 +138,8 @@ let deltaTime = 0;
   const gl = canvas.getContext("webgl");
 
   if (gl === null) {
-    alert(
-      "Unable to initialize WebGL. Your browser or machine may not support it."
-    );
+    alert("Unable to initialize WebGL. Your "     +
+          "browser or machine may not support it.");
     return;
   }
 
@@ -202,7 +172,8 @@ let deltaTime = 0;
     }
   `;
 
-  function initShaderProgram(gl, vsSource, fsSource) {
+  let shaderProgram;
+  {
     function loadShader(gl, type, source) {
       const shader = gl.createShader(type);
       gl.shaderSource(shader, source);
@@ -221,7 +192,7 @@ let deltaTime = 0;
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
 
-    const shaderProgram = gl.createProgram();
+    shaderProgram = gl.createProgram();
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
     gl.linkProgram(shaderProgram);
@@ -234,10 +205,7 @@ let deltaTime = 0;
       );
       return null;
     }
-
-    return shaderProgram;
   }
-  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
 
   const programInfo = {
     program: shaderProgram,
@@ -251,8 +219,6 @@ let deltaTime = 0;
     },
   };
 
-  // Here's where we call the routine that builds all the
-  // objects we'll be drawing.
   const buffers = initBuffers(gl);
 
   let then = 0;
@@ -272,8 +238,10 @@ let deltaTime = 0;
 
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    drawScene(gl, programInfo, buffers, { cubeRotation, color: [0, 0, 0], scale: 1.00 });
-    drawScene(gl, programInfo, buffers, { cubeRotation, color: [1, 1, 1], scale: 0.95 });
+    const g = 0.3;
+    drawScene(gl, programInfo, buffers, { cubeRotation, color: [g, g, g], scale: [2.00, 0.01, 2.00] });
+    drawScene(gl, programInfo, buffers, { cubeRotation, color: [0, 0, 0], scale: [1.00, 1.00, 1.00] });
+    drawScene(gl, programInfo, buffers, { cubeRotation, color: [1, 1, 1], scale: [0.95, 0.95, 0.95] });
     cubeRotation += deltaTime;
   });
 })();
